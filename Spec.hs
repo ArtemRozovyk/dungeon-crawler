d702e4d8d9c6e05ea65f84d16a72f542db7dbe2a
QuentinP34 <60472228+QuentinP34@users.noreply.github.com>
2020-05-13 22:42:09 +0200

    Specs & cie

---
+++ Spec.hs
@@ -0,0 +1,20 @@
+import Test.Hspec
+import GameStateSpec as SS
+
+import Carte 
+import CarteSpec as CS
+
+import Envi
+import EnviSpec as ES
+
+import Model
+import ModeleSpec as MS
+
+
+main :: IO ()
+main = hspec $ do
+  -- revrev
+  CS.engineSpec 
+  SS.add_entity_state_test
+  ES.engineSpec
+  MS.engineSpec
